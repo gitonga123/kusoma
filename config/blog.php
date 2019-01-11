675ca689a8a2a07c43d8563f2f8abae75e5f7c1f
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2019-01-11 07:50:28 +0300

    Custom configurations files, key, env variables

---
+++ config/blog.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Daniel Mutwiri
+ * Date: 11-Jan-19
+ * Time: 7:46 AM
+ */
+
+return [
+    'admins' => [
+        'danmutwiri@yahoo.com',
+        'mutwiridanielsci@gmail.com'
+    ]
+];
